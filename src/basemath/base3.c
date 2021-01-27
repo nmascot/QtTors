nmascot/QtTors

commit b1932a2238d60823d155107021ce654c7412fb4e

21- missing GC in nfeltval + better algorithm when valuation is huge

src/basemath/base3.c
@@ -743,6 +743,7 @@ nfM_nfC_mul(GEN nf, GEN A, GEN B)
 long
 ZC_nfvalrem(GEN x, GEN pr, GEN *newx)
 {
+  pari_sp av = avma;
   long i, v, l;
   GEN r, y, p = pr_get_p(pr), mul = pr_get_tau(pr);
 
@@ -758,6 +759,12 @@ ZC_nfvalrem(GEN x, GEN pr, GEN *newx)
       if (r != gen_0) { if (newx) *newx = x; return v; }
     }
     swap(x, y);
+    if ((v & 0xf) == 0xf) v += pr_get_e(pr) * ZV_pvalrem(x, p, &x);
+    if (gc_needed(av,1))
+    {
+      if(DEBUGMEM>1) pari_warn(warnmem,"ZC_nfvalrem, v >= %ld", v);
+      gerepileall(av, 2, &x, &y);
+    }
   }
 }
 long

commit 374481c7544b0e369107e734096a85a199e6a96a

fix problem in ZC_nfvalrem introduced in b1932a22
we can't remove p-contents when newx is not NULL. We could modify the
function interface to set *newx = x * (tau/p)^v1 / p^v2 with
v_pr(x) = v1 + e(pr/p) * v2; this would be more efficient. Don't bother for now.

src/basemath/base3.c
@@ -759,7 +759,7 @@ ZC_nfvalrem(GEN x, GEN pr, GEN *newx)
       if (r != gen_0) { if (newx) *newx = x; return v; }
     }
     swap(x, y);
-    if ((v & 0xf) == 0xf) v += pr_get_e(pr) * ZV_pvalrem(x, p, &x);
+    if (!newx && (v & 0xf) == 0xf) v += pr_get_e(pr) * ZV_pvalrem(x, p, &x);
     if (gc_needed(av,1))
     {
       if(DEBUGMEM>1) pari_warn(warnmem,"ZC_nfvalrem, v >= %ld", v);