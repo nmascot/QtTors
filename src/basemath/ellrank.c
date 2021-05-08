797d9a4a736d89ca37d49a06644e57a189d5b408
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2021-05-08 02:16:27 +0200

    remove 'lim' argument from liftselmer

--- src/basemath/ellrank.c
+++ src/basemath/ellrank.c
@@ -1276,10 +1276,10 @@ liftselmer_cover(GEN b, GEN expo, GEN LS2, GEN pol, GEN K)
 
 static GEN
 liftselmer(GEN b, GEN expo, GEN sbase, GEN LS2, GEN pol,
-           GEN selmer, GEN K, long lim, long ntry)
+           GEN selmer, GEN K, long ntry)
 {
   pari_sp av = avma, av2;
-  long t;
+  long t, lim = ntry * LIM1;
   GEN ttheta, tttheta, z, polprime;
   z = RgXQV_factorback(LS2, expo, pol);
   ttheta = RgX_shift_shallow(pol,-2);
@@ -1505,7 +1505,7 @@ ell2selmer(GEN ell, GEN ell_K, GEN help, GEN K, GEN vbnf,
     if (Flm_Flc_invimage(newselmer, vec, 2)) continue;
     expo = Flm_Flc_mul(selmer, vec, 2);
     b = liftselmerinit(expo, vnf, sqrtLS2, factLS2, badprimes, vcrt, pol);
-    P = liftselmer(b, expo, sbase, LS2, pol, selmer, K, LIM1, 1);
+    P = liftselmer(b, expo, sbase, LS2, pol, selmer, K, 1);
     if (P)
     {
       gel(listpoints, ++nbpoints) = P; /* new point on ell */
@@ -1521,7 +1521,7 @@ ell2selmer(GEN ell, GEN ell_K, GEN help, GEN K, GEN vbnf,
     while (zv_equal0(vec) || Flm_Flc_invimage(newselmer, vec, 2));
     expo = Flm_Flc_mul(selmer, vec, 2);
     b = liftselmerinit(expo, vnf, sqrtLS2, factLS2, badprimes, vcrt, pol);
-    P = liftselmer(b, expo, sbase, LS2, pol, selmer, K, u*LIM1, u);
+    P = liftselmer(b, expo, sbase, LS2, pol, selmer, K, u);
     if (P)
     {
       gel(listpoints, ++nbpoints) = P;
