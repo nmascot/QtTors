ac62ff3946e9dffdb27475af41453809d3905bcb
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2021-06-07 21:09:55 +0200

    ellQ_setprec: remove spurious declaration

--- src/headers/paridecl.h
+++ src/headers/paridecl.h
@@ -3077,7 +3077,6 @@ GEN     ellfromeqn(GEN s);
 /* elliptic.c */
 enum { t_ELL_Rg = 0, t_ELL_Q, t_ELL_Qp, t_ELL_Fp, t_ELL_Fq, t_ELL_NF };
 long    ellQ_get_CM(GEN e);
-void    ellQ_setprec(long prec);
 int     ell_is_integral(GEN E);
 GEN     ellbasechar(GEN E);
 GEN     akell(GEN e, GEN n);
