4cef71d49357b21371784960f239ef79ab96930d
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2021-07-05 18:18:03 +0200

    nfinitall no longer exists

--- src/headers/paridecl.h
+++ src/headers/paridecl.h
@@ -2199,7 +2199,6 @@ void    nfinit_basic(nfmaxord_t *T, GEN x);
 GEN     nfinit_complete(nfmaxord_t *T, long flag, long prec);
 GEN     nfinit(GEN x, long prec);
 GEN     nfinit0(GEN x, long flag, long prec);
-GEN     nfinitall(GEN x, long flag, long prec);
 GEN     nfinitred(GEN x, long prec);
 GEN     nfinitred2(GEN x, long prec);
 GEN     nfisincl(GEN a, GEN b);
