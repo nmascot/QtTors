24c229899e0fab9d4f8ed17d992407c17e2f69ba
Bill Allombert <Bill.Allombert@math.u-bordeaux.fr>
2021-09-28 19:52:53 +0200

    has_dlopen.c: also check for dlsym
    This is needed to detect -ldl when using ASAN (-fsanitize) with some versions
    of gcc.

--- config/has_dlopen.c
+++ config/has_dlopen.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <dlfcn.h>
-int main() {dlopen("a",RTLD_LAZY); return 0;}
+
+int main()
+{
+  void *handle = dlopen(NULL,RTLD_LAZY);
+  if (handle)
+    printf("%p\n",dlsym(handle, "fun"));
+  return 0;
+}
