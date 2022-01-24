545b471bf48d8bc68a362fb813cea3799e7a4033
Bill Allombert <Bill.Allombert@math.u-bordeaux.fr>
2022-01-24 17:20:52 +0100

    config/has_*.c: do not use ulong. Might or might not be defined yet.
    On linux, ulong is defined by sys/types.h which is pulled by most other
    headers.

--- config/has_avx.c
+++ config/has_avx.c
@@ -1,8 +1,8 @@
 #include <immintrin.h>
  /* Use AVX 256 bit registers for the bit arrays */
-typedef ulong bit_array __attribute__ ((vector_size (32)));
-#define EXT0(a) ((ulong)a[0])
-#define EXT(a,i) ((ulong)a[i])
+typedef unsigned long bit_array __attribute__ ((vector_size (32)));
+#define EXT0(a) ((unsigned long)a[0])
+#define EXT(a,i) ((unsigned long)a[i])
 #ifdef __AVX2__
 #define TEST(a) ( _mm256_movemask_epi8(_mm256_cmpeq_epi8((__m256i)(a), (__m256i)RBA(0))) != 0xffffffffU )
 #elif defined(__AVX__)
@@ -10,11 +10,11 @@ typedef ulong bit_array __attribute__ ((vector_size (32)));
 #else
 #define TEST(a) (EXT(a,0) || EXT(a,1) || EXT(a,2) || EXT(a,3))
 #endif
-#define RBA(a) ((bit_array){((ulong) a), ((ulong) a), ((ulong) a), ((ulong) a)})
+#define RBA(a) ((bit_array){((unsigned long) a), ((unsigned long) a), ((unsigned long) a), ((unsigned long) a)})
 int main(void)
 {
   bit_array x = RBA(1L), y = RBA(3L);
-  ulong t = TEST(x&y);
+  unsigned long t = TEST(x&y);
   (void) t;
   return 0;
 }
--- config/has_neon.c
+++ config/has_neon.c
@@ -1,6 +1,6 @@
-typedef ulong bit_array __attribute__ ((vector_size (16)));
-#define EXT0(a) ((ulong)a[0])
-#define EXT(a,i) ((ulong)a[i])
+typedef unsigned long bit_array __attribute__ ((vector_size (16)));
+#define EXT0(a) ((unsigned long)a[0])
+#define EXT(a,i) ((unsigned long)a[i])
 #define AND(a,b) ((a)&(b))
 #define TEST(a) (EXT0(a) || EXT1(a))
 #define RBA(a) ((bit_array){((long) a), ((long) a)})
@@ -8,7 +8,7 @@ typedef ulong bit_array __attribute__ ((vector_size (16)));
 int main(void)
 {
   bit_array x = RBA(1L), y = RBA(3L);
-  ulong t = TEST(AND(x,y));
+  unsigned long t = TEST(AND(x,y));
   (void) t;
   return 0;
 }
