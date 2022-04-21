4d847aa6bfb2b2bbb9bb2650b096332313de6edd
Bill Allombert <Bill.Allombert@math.u-bordeaux.fr>
2022-04-21 11:27:45 +0200

    Remove unused private function hyperell_redsl2
    (superseded by hyperellreduce)

--- src/headers/paripriv.h
+++ src/headers/paripriv.h
@@ -873,7 +873,6 @@ hashtable *hashstr_import_static(hashentry *e, ulong size);
 /* hyperell.c */
 
 GEN     ZlXQX_hyperellpadicfrobenius(GEN H, GEN T, ulong p, long n);
-GEN     hyperell_redsl2(GEN Q);
 
 /* ifactor1.c */
 
