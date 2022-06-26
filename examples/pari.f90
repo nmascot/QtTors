92a5131c141e3c98b8028ea363fbc59650577601
Bill Allombert <Bill.Allombert@math.u-bordeaux.fr>
2022-06-26 18:12:31 +0200

    pari.f90: add example of garbage collecting

--- examples/pari.f90
+++ examples/pari.f90
@@ -11,6 +11,15 @@ module PARI
     subroutine pari_close() bind(C,name='pari_close')
     end subroutine pari_close
     !
+    subroutine set_avma( av ) bind(C,name='set_avma')
+      import C_LONG
+      integer(kind=C_LONG), VALUE  :: av
+    end subroutine set_avma
+    !
+    integer(kind=C_LONG) function get_avma( ) bind(C,name='get_avma')
+      import C_LONG
+    end function get_avma
+    !
     type(C_PTR) function dbltor( r ) bind(C,name='dbltor')
       import C_DOUBLE, C_PTR
       real(kind=C_DOUBLE), VALUE  :: r
@@ -65,20 +74,30 @@ module PARI
       integer(kind=C_LONG), VALUE :: x
       integer(kind=C_LONG), VALUE :: prec
     end function Pi2n
+    type(C_PTR) function gerepilecopy(av, z) bind(C,name='gerepilecopy')
+      import C_PTR, C_LONG
+      integer(kind=C_LONG), VALUE :: av
+      type(C_PTR), VALUE :: z
+    end function gerepilecopy
  end interface
 end module PARI
 !
 PROGRAM prog
-  use ISO_C_BINDING, only : C_PTR, C_DOUBLE
   use PARI
   implicit none
-  real(kind=C_DOUBLE) :: r      = 1e36
+  real(kind=C_DOUBLE) :: r
   type(C_PTR)         :: p
+  integer(kind=C_LONG) :: i
+  integer(kind=C_LONG) :: av
   integer(kind=C_LONG) :: prec = 20 ! 18 words
   CALL pari_init(10000000_8,2_8)
-  p = glog(stoi(10000_8),prec)
-  p = gmod(p, Pi2n(1_8, prec))
-  r = rtodbl(p)
+  av = get_avma()
+  do i=1_8,10_8
+    p = glog(stoi(i*1000),prec)
+    p = gmod(p, Pi2n(1_8, prec))
+    r = rtodbl(p)
+    PRINT '(a,i10,a,f0.9)','log(',i*1000,')%(2*Pi)  = ', r
+    CALL set_avma(av)
+  enddo
   CALL pari_close()
-  PRINT '(a,f0.9)','log(10000)%(2*Pi)  = ', r
 END PROGRAM prog
