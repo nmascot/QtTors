e9dbdfee21d70689f8d484364807c6f215bd714d
Aurel Page <aurel.page@normalesup.org>
2023-04-26 10:07:49 +0200

    fix rnf/gchar confusion in get_nf (#2473)

--- src/basemath/base1.c
+++ src/basemath/base1.c
@@ -35,9 +35,9 @@ rawchecknf(GEN v) { return typ(v)==t_VEC && lg(v)==10; }
 /* v a t_VEC, lg(v) = 11, sanity check for true bnf */
 static int
 v11checkbnf(GEN v) { return rawchecknf(bnf_get_nf(v)); }
-/* v a t_VEC, lg(v) = 12, sanity check for true bnf */
+/* v a t_VEC, lg(v) = 12, sanity check for true nf and true bnf */
 static int
-v13checkgchar(GEN v) { return rawcheckbnf(gchar_get_bnf(v)); }
+v13checkgchar(GEN v) { return rawchecknf(gchar_get_nf(v)) && rawcheckbnf(gchar_get_bnf(v)); }
 /* v a t_VEC, lg(v) = 10, sanity check for true nf */
 static int
 v10checknf(GEN v) { return typ(gel(v,1))==t_POL; }
